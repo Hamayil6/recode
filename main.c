3aff770203853d02a7b76f5de1ec9d53439e6f5a
Hamayil6 <hamayilaliyeva6@gmail.com>
2024-05-25 13:30:05 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
-int main() {
+int main()
+{
   int i;
-  for (i = 0; i < 32; i++)  {
-    printf("Hello School\n");
+  for (i = 0; i < 32; i++) // Change 10 to 32
+  {
+    printf("Hello School\n"); // Change "School" to "Hello School"
   }
   return 0;
 }
+
